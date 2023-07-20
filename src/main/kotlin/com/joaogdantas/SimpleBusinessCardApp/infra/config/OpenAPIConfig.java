f6824720da062198da7db28dbad0712b9e5d358c
joaogdantas <joaogdantasdev@hotmail.com>
2023-07-20 14:27:41 -0300

    FEATURE: Added Swagger config

---
+++ src/main/kotlin/com/joaogdantas/SimpleBusinessCardApp/infra/config/OpenAPIConfig.java
@@ -0,0 +1,44 @@
+package com.joaogdantas.SimpleBusinessCardApp.infra.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${joaogdantas.openapi.dev-url}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("joaogdantasdev@hotmail.com");
+        contact.setName("João Gabriel");
+        contact.setUrl("https://www.linkedin.com/in/joaogdantas/");
+
+        License License = new License().name("Licença - João Gabriel").url("https://www.linkedin.com/in/joaogdantas/");
+
+        Info info = new Info()
+                .title("Quiz Quiz App API")
+                .version("0.0.1")
+                .contact(contact)
+                .description("Uma API para um app de criação de cartões de visita.").termsOfService("https://github.com/joaogdantas/BusinessCardAPI")
+                .license(License);
+
+        return new OpenAPI().info(info).servers(List.of(devServer));
+    }
+}
+
