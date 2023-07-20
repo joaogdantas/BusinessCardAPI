154725ba27913fe288748a82854b47a55da2eb05
joaogdantas <joaogdantasdev@hotmail.com>
2023-07-20 14:25:29 -0300

    add project basic structure

---
+++ src/main/kotlin/com/joaogdantas/SimpleBusinessCardApp/SimpleBusinessCardAppApplication.kt
@@ -0,0 +1,11 @@
+package com.joaogdantas.SimpleBusinessCardApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleBusinessCardAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleBusinessCardAppApplication>(*args)
+}
